7f4f764d27bf5a518e074a72bf04083491d3fc91
arthurom254 <99343006+arthurom254@users.noreply.github.com>
2023-10-26 07:33:45 +0300

    Update settings.py

--- Arthur/settings.py
+++ Arthur/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
 # 	'ENGINE':'django.db.backends.postgresql',
 # 	'NAME':'postgres',
 # 	'USER':'postgres',
-# 	'PASSWORD':'Poweredge123_',
+# 	'PASSWORD':'',
 # 	'HOST':'localhost',
 # 	'PORT':'5432',
 #     }
